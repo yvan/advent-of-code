bd34914e46129bf5c94c3f1f4c66c6eaeacf7961
yvan <yvanscher@gmail.com>
2020-05-15 22:12:54 -0400

    fixed runtime for d2

--- d2.lisp
+++ d2.lisp
@@ -9,9 +9,9 @@
   program)
 (defun get-pos (pos program)
   (list (nth (+ pos 1) program) (nth (+ pos 2) program) (nth (+ pos 3) program)))
-(defun replace-positions (program)
-  (setf (nth 1 program) 12
-    (nth 2 program) 2)
+(defun replace-positions (program noun verb)
+  (setf (nth 1 program) noun
+	(nth 2 program) verb)
   program)
 (defun process-program (program pos)
   (let* ((poss (get-pos pos program))
@@ -28,18 +28,18 @@
         (+ pos 4)))
       ((eq (nth pos program) 99)
        (nth 0 program)))))
-(process-program (replace-positions (read-file2 "data/d2.txt")) 0)
+(process-program (replace-positions (read-file2 "data/d2.txt") 12 2) 0)
 
 (defun solve-noun-verb (objective input-space raw-program)
   (remove-if #'null (loop for input in input-space
-              collect (let* ((test-noun (car input))
-                     (test-verb (cdr input))
-                     (program (replace-positions raw-program test-noun test-verb))
-                     (result-prog (process-program (copy-list program) 0)))
-                (if (eq result-prog objective)
-                    (return `(,input ,result-prog)))))))
+		       collect (let* ((test-noun (car input))
+				      (test-verb (cdr input))
+				      (program (replace-positions raw-program test-noun test-verb))
+				      (result-prog (process-program (copy-list program) 0)))
+				 (if (eq result-prog objective)
+				     (return `(,input ,result-prog)))))))
 (defun create-nbyk-input-space (n k)
   (loop for i from 0 to n
      append (loop for j from 0 to k
            collect `(,i . ,j))))
-(solve-noun-verb 19690720 (create-nbyk-input-space 99 99) (read-file2 "data/d2.txt")
+(solve-noun-verb 19690720 (create-nbyk-input-space 99 99) (read-file2 "data/d2.txt"))
