4b1c89e17877c266a8847582cfdf53a4e66017d9
yvan <yvanscher@gmail.com>
2020-05-15 22:50:36 -0400

    update d3

--- d3.lisp
+++ d3.lisp
@@ -1,4 +1,4 @@
-(ql:quickload :split-sequence)
+(ql:quickload "str")
 (defun file-get-lines (filename)
   (with-open-file (stream filename)
     (loop for line = (read-line stream nil)
@@ -6,7 +6,7 @@
          collect (split-sequence:split-sequence #\, line))))
 ; take an initial point and a
 ; puzzle component and return
-; the points for htat puzzle component
+; the points for that puzzle component
 ; ((0 . 0) "R55") -> ((0 . 0) (0 . 1)... (0 . 54))
 (defun build-points-helper (x-y current-point start end)
   (if (> end start) 
@@ -14,7 +14,7 @@
          collect (if (equal x-y #\x) (cons i (cdr current-point)) (cons (car current-point) i)))
       (loop for i from start downto end 
          collect (if (equal x-y #\x) (cons i (cdr current-point)) (cons (car current-point) i)))))
-(defun build-points (current-point circuit-point-list)
+(defun build-points (current-point circuit-point-list &optional tries max_tries)
   (if (> (car current-point) 1000) 
       '()
       (let* ((circuit-point (car circuit-point-list))
@@ -24,7 +24,8 @@
              (sign (if (member direction '(#\R #\U)) 1 -1))
              (start (if (equal x-y #\x) (car current-point) (cdr current-point)))
              (end (+ (* end-value sign) start))
-             (new-points (build-points-helper x-y current-point start end)))
+             (new-points (build-points-helper x-y current-point start end))
+	     (tries (1+ tries)))
         (print current-point)
         (print circuit-point)
         (print direction)
@@ -33,11 +34,12 @@
         (print sign)
         (print start)
         (print end)
-        (append new-points (build-points (car (last new-points)) (cdr circuit-point-list))))))
+	(if (< tries max_tries)
+	    (append new-points (build-points (car (last new-points)) (cdr circuit-point-list) tries max_tries))))))
 (defun find-intersection-points (nodes-1 nodes-2)
   (loop for node in nodes-1
      collect (car (remove-if-not #'(lambda (x) (equal node x)) nodes-2))))
-;; (defparameter *nodes1* (build-points (cons 0 0) (car (file-get-lines "d3.txt"))))
-;; (defparameter *nodes2* (build-points (cons 0 0) (cadr (file-get-lines "d3.txt"))))
-;; (defparameter *intersect* (remove-if #'null (find-intersection-points *nodes1* *nodes2*)))
+(defparameter *nodes1* (build-points (cons 0 0) (car (file-get-lines "data/d3.txt")) 0 10))
+(defparameter *nodes2* (build-points (cons 0 0) (cadr (file-get-lines "data/d3.txt"))))
+(defparameter *intersect* (remove-if #'null (find-intersection-points *nodes1* *nodes2*)))
 ;; (reduce #'m
