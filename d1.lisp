13cb9a04a491c01a14b7a56be972f05a2e9d943f
yvan <yvanscher@gmail.com>
2020-06-05 09:48:13 -0400

    update

--- d1.lisp
+++ d1.lisp
@@ -1,19 +1,20 @@
-;;read in data
+;; pt1
 (defun read-file (file)
   (with-open-file (stream file)
     (loop for line = (read-line stream nil)
        while line
        collect (parse-integer line))))
-;; calculate fuel cost
+
 (defun calc-fuel-cost (mass)
   (- (floor (/ mass 3)) 2))
-;; apply this to the data
-(apply #'+ (mapcar #'calc-fuel-cost (read-file "data/d1.txt")))
-;; calcualate module fuel
+
+(print (reduce '+ (mapcar #'calc-fuel-cost (read-file "data/d1.txt"))))
+
+;; pt2
 (defun calc-mod-fuel (module-size)
   (let ((module-fuel (calc-fuel-cost module-size)))
     (if (<= module-fuel 0)
         0
         (+ module-fuel (calc-mod-fuel module-fuel)))))
-;; calculate additional fuel
-(apply #'+ (mapcar #'calc-mod-fuel (read-file "data/d1.txt")))
+
+(print  (reduce '+ (mapcar #'calc-mod-fuel (read-file "data/d1.txt"))))
